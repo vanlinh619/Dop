9063cb0f660adf48e06684aefa87506c1ed01bd1
linh <lvlinh619@gmail.com>
2024-12-17 22:56:20 +0700

    config public client for SPA

--- Dop/src/main/java/org/dop/module/startup/registeredclient/Oauth2RegisteredClientStarter.java
+++ Dop/src/main/java/org/dop/module/startup/registeredclient/Oauth2RegisteredClientStarter.java
@@ -1,6 +1,7 @@
 package org.dop.module.startup.registeredclient;
 
 import lombok.RequiredArgsConstructor;
+import org.dop.config.property.ManagePageProperties;
 import org.dop.entity.state.StartupName;
 import org.dop.module.startup.Starter;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
@@ -20,18 +21,24 @@ public class Oauth2RegisteredClientStarter implements Starter {
     public static final String MASTER_CLIENT = "master-client";
 
     private final RegisteredClientRepository registeredClientRepository;
+    private final ManagePageProperties managePageProperties;
 
+    /**
+     * Create default public client, that can use by Single Page Application
+     */
     @Override
     public void start() {
         RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
                 .clientId(MASTER_CLIENT)
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .scope(OidcScopes.OPENID)
                 .scope(OidcScopes.PROFILE)
-                .scope(OidcScopes.EMAIL)
-                .clientSettings(ClientSettings.builder().build())
+                .redirectUri(managePageProperties.getRootUrl())
+                .clientSettings(ClientSettings.builder()
+                        .requireProofKey(true)
+                        .build())
                 .build();
 
         registeredClientRepository.save(oidcClient);
